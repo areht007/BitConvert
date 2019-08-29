437e2e40c48c30a61a39e33542ee1eba82f866f4
Zhassulan Aimukhambetov <aimzhassulan@icloud.com>
2019-08-29 09:17:32 +0600

    Initial Commit

---
+++ lesson3.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lesson3.1
+//
+//  Created by Zhassulan Aimukhambetov on 8/29/19.
+//  Copyright © 2019 Zhassulan Aimukhambetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
